1f18dda4be1b0051dd74ac2d4853820b78215ea5
Matt Lahey <mdlahey@outlook.com>
2022-10-07 14:42:26 -0400

    added an array of question objects to the questions js file

---
+++ Assets/JS/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: "Which of the following function of String object executes the search for a match between a regular expression and a specified string?",
+        choices: ["concat()", "match()", "replace()", "search()"],
+        answer: "search()"
+    },
+    {
+        title: "Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
+        choices: ["getIndex()", "location()", "indexOf()", "None of the above"],
+        answer: "indexOf()"
+    },
+    {
+        title: "Which method would you use to find an ID element",
+        choices: ["getElementsById()", "getElementByID()", "getElementbyId()", "getElementsById()"],
+        answer: "getElementbyId()"
+    },
+    {
+        title: "Which of the following function of Array object calls a function for each element in the array?",
+        choices: ["concat()", "every()", "filter()", "forEach()"],
+        answer: "forEach()"
+    },
+    {
+        title: "The appendChild() method places a node as the ____ child.",
+        choices: ["last place you left off", "first", "random", "last"],
+        answer: "last"
+    },
+    {
+        title: "Which of the following variables takes precedence over the others if the names are the same?",
+        choices: ["Global variable", "The local element", "Variables in parenthesis", "None of the above"],
+        answer: "The local element"
+    },
+    {
+        title: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["=", "'equals'", "==", "!="],
+        answer: "=="
+    },
+    {
+        title: "Javascript variables can be ______",
+        choices: ["locally scoped", "function scoped", "globally scoped", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "What type of langauge is JavaScript?",
+        choices: ["Object-Oriented", "Object-Based", "Assembly-langauge", "High-level"],
+        answer: "Object-Based"
+    },
+    {
+        title: "The 'function' and 'var' are known as:",
+        choices: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
+        answer: "Declaration statements"
+    },
+];
