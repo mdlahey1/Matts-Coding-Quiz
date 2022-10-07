b6de257a60fd20bffbe55a45358a5920fd989d2e
Matt Lahey <mdlahey@outlook.com>
2022-10-07 16:47:33 -0400

    created variables for relevant elements

---
+++ Assets/JS/script.js
@@ -0,0 +1,11 @@
+//Declare variables for relevant elements
+var choicesOptions = document.querySelector('#choicesOptions');
+var startMenu = document.getElementById('startMenu');
+var questionHeader = document.getElementById('questionHeader');
+var gameClock = document.getElementById('gameClock');
+var initialsForm = document.getElementById('initialsForm');
+var initialsInput = document.getElementById('initialsInput');
+var submitInitialsBtn = document.getElementById('submitInitialsBtn');
+var highScoresLink = document.getElementById('highScoresLink');
+var backToStart = document.getElementById('backToStart');
+var highScoresList = document.getElementById('highScoresList');
