620c4b302f45f9d53dbeac3d16e08392b71af78d
Matt Lahey <mdlahey@outlook.com>
2022-10-10 11:35:25 -0400

    added logic to evaluate/notify the user's answer selections

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -38,7 +38,7 @@ function renderHighScores() {
     highScores = scoresObject;
 }
 
-//Create function for when user clicks the "Start Quiz" button
+//Creates a function for when user clicks the "Start Quiz" button
 function startQuiz() {
     console.log("Question #: " + questionNumber);
 
@@ -57,7 +57,7 @@ function startQuiz() {
     listQuestion();
 }
 
-//Create function that loops through the possible answers for each question and creates a button for each answer
+//Creates a function that loops through the possible answers for each question and creates a button for each answer
 function listQuestion() {
     //Update question title
     questionHeader.textContent = questions[questionNumber].title;
@@ -71,7 +71,7 @@ function listQuestion() {
     }
 }
 
-//Create function for clock countdown
+//Creates a function for clock countdown
 function countdownClock() {
     var clockTick = setInterval(function() {
         //Decrease the clockTime variable every second and update the display
@@ -97,3 +97,77 @@ function countdownClock() {
         }
     }, 1000);
 }
+
+//Creates a function that notifies the user they chose the correct answer
+function correctAnswer() {
+    var correctNotification = document.createElement("section");
+    correctNotification.setAttribute("class", "border-top mt-4 pt-4");
+    correctNotification.setAttribute("style", "font-size: 12px; color: green; font-weight: bold;");
+    correctNotification.textContent = "You selected the correct answer!";
+    choicesOptions.appendChild(correctNotification);
+}
+
+//Creates a function that notifies the user they chose an incorrect answer
+function incorrectAnswer() {
+    var incorrectNotification = document.createElement("section");
+    incorrectNotification.setAttribute("class", "border-top mt-4 pt-4");
+    incorrectNotification.setAttribute("style", "font-size: 12px; color: red; font-weight: bold;");
+    incorrectNotification.textContent = "You selected an incorrect answer";
+    choicesOptions.appendChild(incorrectNotification);
+}
+
+//Create an event listener with a function to evaluate whether or not the user has chosen the correct answer and notifies them/moves on to the next question
+document.addEventListener("click", function(event) {
+    if (event.target.matches(".choice-btn")) {
+        console.log(event.target.textContent);
+        event.stopPropagation();
+        event.preventDefault();
+        //Correct answer condition
+        if (event.target.textContent === questions[questionNumber].answer) {
+            //Move on to the next question and add time to the game clock
+            questionNumber = questionNumber + 1;
+            clockTime = clockTime + 5;
+            
+            //Checks if there are any more questions and if so clears button contents/creates new question/answers and notifies the user if they got the answer right/wrong
+            if (questionNumber <= totalQuestions-1) {
+                choicesOptions.innerHTML = " ";
+                listQuestion();
+                correctAnswer();
+            } else {
+                //End quiz and notify user of correct answer on last question
+                choicesOptions.innerHTML = " ";
+                correctAnswer();
+                //Display initials input form to allow user to enter high score
+                initialsForm.setAttribute("style", "display: block;");
+                //Allow user to restart quiz and display their final score
+                startMenu.setAttribute("style", "display: block;");
+                highScoresLink.setAttribute("style", "display: inline;");
+                questionHeader.textContent = "Your score is: " + clockTime;
+                finalScore = clockTime;
+            }    
+        }
+        else if (event.target.textContent !== questions[questionNumber].answer) {
+            //Move on to the next question and add time to the game clock
+            questionNumber = questionNumber + 1;
+            clockTime = clockTime - 10;
+
+            //Checks if there are any more questions and if so clears button contents/creates new question/answers and notifies the user if they got the answer right/wrong
+            if (questionNumber <= totalQuestions-1) {
+                choicesOptions.innerHTML = " ";
+                listQuestion();
+                incorrectAnswer();
+            } else {
+                //End quiz and notify user of correct answer on last question
+                choicesOptions.innerHTML = " ";
+                incorrectAnswer();
+                //Display initials input form to allow user to enter high score
+                initialsForm.setAttribute("style", "display: block;");
+                //Allow user to restart quiz and display their final score
+                startMenu.setAttribute("style", "display: block;");
+                highScoresLink.setAttribute("style", "display: inline;");
+                questionHeader.textContent = "Your score is: " + clockTime;
+                finalScore = clockTime;
+            }
+        }     
+    }
+});
